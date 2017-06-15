047e3ab583361d2e0443f361b29f5ef0f445d54b
MarsonShine <ms27946@163.com>
2017-06-15 23:56:57 +0800

    设计模式
    设计模式练习

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.Decorator;
+using DesignPattern.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DECORATOR
+            Person ms = new Person("MarsonShine");
+            Console.WriteLine("\n 第一种妆扮：");
+            TShirts dtx = new TShirts();
+            BigTrouser bt = new BigTrouser();
+            dtx.Decorate(ms);
+            bt.Decorate(dtx);
+            bt.Show(); 
+#endif
+            SchoolGirl zhuqin = new SchoolGirl();
+            zhuqin.Name = "祝琴";
+            Proxy.Proxy ms = new Proxy.Proxy(zhuqin);
+            ms.GiveChocolate();
+            ms.GiveDolls();
+            ms.GiveFlowers();
+            Console.ReadLine();
+        }
+    }
+}

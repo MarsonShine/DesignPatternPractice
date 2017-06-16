927a7d22f0c4a871da47cbf853d78ec636e5ad1b
MarsonShine <ms27946@163.com>
2017-06-17 01:12:33 +0800

    update design pattern

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,6 @@
-﻿using DesignPattern.Decorator;
+﻿using DesignPattern.Bridge;
+using DesignPattern.ChainOfResponsibility;
+using DesignPattern.Decorator;
 using DesignPattern.Proxy;
 using System;
 using System.Collections.Generic;
@@ -21,13 +23,57 @@ namespace DesignPattern
             bt.Decorate(dtx);
             bt.Show(); 
 #endif
+#if Proxy
             SchoolGirl zhuqin = new SchoolGirl();
             zhuqin.Name = "祝琴";
             Proxy.Proxy ms = new Proxy.Proxy(zhuqin);
             ms.GiveChocolate();
             ms.GiveDolls();
             ms.GiveFlowers();
-            Console.ReadLine();
+            Console.ReadLine(); 
+#endif
+
+#if ChanOfResposibility
+            HandsetBrand hb;
+            hb = new HandsetBrandN();
+            hb.SetHandsetSoft(new HandsetGame());
+            hb.Run();
+
+            hb.SetHandsetSoft(new HandsetAddressList());
+            hb.Run();
+
+            HandsetBrand hb2;
+            hb2 = new HandsetBrandM();
+            hb2.SetHandsetSoft(new HandsetGame());
+            hb2.Run();
+
+            hb2.SetHandsetSoft(new HandsetAddressList());
+            hb2.Run(); 
+#endif
+            CommonManager jinli = new CommonManager("jinli");
+            Majordomo zongjian = new Majordomo("zongjian");
+            GeneralManager zhongjingli = new GeneralManager("zhongjinli");
+            jinli.SetSuperior(jinli);
+            zongjian.SetSuperior(zhongjingli);
+
+            Request request = new Request();
+            request.RequestType = "请假";
+            request.RequestContent = "我要请假";
+            request.Number = 1;
+            jinli.RequestApplications(request);
+
+            Request request2 = new Request();
+            request2.RequestType = "请假";
+            request2.RequestContent = "我要请假";
+            request.Number = 4;
+            jinli.RequestApplications(request2);
+
+            Request request3 = new Request();
+            request3.RequestType = "请假";
+            request3.RequestContent = "我还是要请假";
+            request.Number = 500;
+            jinli.RequestApplications(request3);
+
         }
     }
 }
